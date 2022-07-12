70369db056600748410e5dfa2f90ff2c8531fd5b
Mikail <feelfantastic42@gmail.com>
2022-07-12 11:21:53 +0800

    move to hennessy

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-ifneq ($(filter $(TARGET_DEVICE),hermes),)
+ifneq ($(filter $(TARGET_DEVICE),hennessy),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(CLEAR_VARS)
 endif
